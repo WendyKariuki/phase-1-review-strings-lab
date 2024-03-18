906c808cffe069e3edc774cfd99886a0111bea9a
WendyKariuki <phaminuswendy@gmail.com>
2024-03-18 15:01:13 +0300

    JS lab2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hoper';
+
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hoper}!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
